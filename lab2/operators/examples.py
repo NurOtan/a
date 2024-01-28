8b7a53b0fac92ebcaf3cc54790c65c549f9487df
Дамир Оралбай <andeka777qqq@gmail.com>
2024-01-28 14:52:29 +0600

    pp2

---
+++ lab2/operators/examples.py
@@ -0,0 +1,162 @@
+#1
+print(10 + 5)
+print("")
+#2 	
+x = 5
+y = 3
+
+print(x + y)
+print("")
+#3 
+x = 5
+y = 3
+
+print(x - y)
+#4 
+x = 5
+y = 3
+
+print(x * y)
+print("")
+#5 
+x = 12
+y = 3
+
+print(x / y)
+print("")
+#6 
+x = 5
+y = 2
+
+print(x % y)
+print("")
+#7 
+x = 2
+y = 5
+
+print(x ** y) 
+print("")
+#8 
+x = 15
+y = 2
+
+print(x // y) 
+print("")
+#9
+x = 5
+
+x += 3
+
+print(x)
+
+#10
+x = 5
+
+x -= 3
+
+print(x)
+#11
+x = 5
+
+x *= 3
+
+print(x)
+#12
+x = 5
+
+x /= 3
+
+print(x)
+#13
+x = 5
+
+x%=3
+
+print(x)
+#14
+x = 5
+
+x//=3
+
+print(x)
+#15
+x = 5
+
+x **= 3
+
+print(x)
+#16
+x = 5
+
+x &= 3
+
+print(x)
+#17
+x = 5
+
+x |= 3
+
+print(x)
+#18
+x = 5
+
+x ^= 3
+
+print(x)
+#19
+x = 5
+
+x >>= 3
+
+print(x) 
+#20
+x = 5
+
+x <<= 3
+
+print(x) 
+#21
+x = 5
+
+print(not(x > 3 and x < 10))
+
+
+#21
+print((6 + 3) - (6 + 3))
+#22
+print(100 + 5 * 3)
+#23
+	
+~x
+#24
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+
+
+
+print(x is y)
+
+
+
+print(x == y)
+
+
+#25
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is not z)
+
+
+print(x is not y)
+
+
+print(x != y)
+
+
+#26
+print(5 + 4 - 7 + 3)
